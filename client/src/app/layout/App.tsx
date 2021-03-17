d8960b6241ce680955f08e2f9997b15b08adb15d
Yov00 <yovelincho@gmail.com>
2021-03-17 15:44:09 +0200

    Error handling handled

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,14 +4,20 @@ import Navbar from './Navbar';
 import ActivityDashboard from '../../features/activities/dashboard/ActivityDashboard';
 import { observer } from 'mobx-react-lite';
 import HomePage from '../../features/home/HomePage';
-import { Route, useLocation } from 'react-router-dom';
+import { Route, Switch, useLocation } from 'react-router-dom';
 import ActivityForm from '../../features/activities/form/ActivityForm';
 import ActivityDetails from '../../features/activities/details/ActivityDetails';
+import TestErrors from '../../features/errors/TestError';
+import { ToastContainer } from 'react-toastify';
+import NotFound from '../../features/errors/NotFound';
+import ServerError from '../../features/errors/ServerError';
 
 function App() {
 const location = useLocation();
   return (
     <Fragment>
+      <ToastContainer position="bottom-right" hideProgressBar/>
+
       <Route exact path='/' component={HomePage}/>
       
       <Route
@@ -20,10 +26,15 @@ const location = useLocation();
           <Fragment>
             <Navbar />
             <Container style={{marginTop:'7em'}}>
-            <Route exact path='/activities' component={ActivityDashboard}/>
-            <Route path='/activities/:id' component={ActivityDetails}/>
-            <Route key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm}/>
-            </Container>
+            <Switch>
+              <Route exact path='/activities' component={ActivityDashboard}/>
+              <Route path='/activities/:id' component={ActivityDetails}/>
+              <Route key={location.key} path={['/createActivity','/manage/:id']} component={ActivityForm}/>
+              <Route path="/errors" component={TestErrors}/>
+              <Route path="/server-error" component={ServerError}/>
+              <Route component={NotFound} />
+            </Switch>
+           </Container>
           </Fragment>
         )}
       />
